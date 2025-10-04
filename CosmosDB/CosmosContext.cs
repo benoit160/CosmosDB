eb1ae90f5707670fcd115e8f6791e73a45df731b
Benoît <benoit160@live.be>
2025-10-04 19:08:51 +0200

    wip

--- CosmosDB/CosmosContext.cs
+++ CosmosDB/CosmosContext.cs
@@ -71,11 +71,11 @@ public class CosmosContext : DbContext
     /// </summary>
     /// <param name="id">The identifier of the function, used to invoke it in queries.</param>
     /// <param name="body">The JavaScript implementation of the function.</param>
+    /// <returns>A <see cref="UserDefinedFunctionProperties"/> object containing the definition of the UDF, to be added to the scripts of a container.</returns>
     /// <remarks>
     /// <para>User-defined functions (UDFs) are used to extend the Azure Cosmos DB for NoSQL’s query language grammar and implement custom business logic.</para>
     /// <para>UDFs can only be called from inside queries as they enhance and extend the SQL query language.</para>
     /// </remarks>
-    /// <returns>The object containing the definition of the UDF, to be added to the scripts of a container.</returns>
     /// <example>
     /// The following example shows how to create a UDF that returns a greeting message.
     /// <code>
@@ -96,13 +96,75 @@ public class CosmosContext : DbContext
 
     public async Task AddUserDefinedFunction()
     {
-        string udfBody = "function SayHi(name){return \"Hi \" + name;}";
+        string udfBody = """
+                         function SayHi(name) {
+                             return "Hi " + name
+                         }
+                         """;
         
         UserDefinedFunctionProperties udf = CreateUserDefinedFunction("SayHi", udfBody);
         
         Container container = GetContainer<Person>();
         
         await container.Scripts.CreateUserDefinedFunctionAsync(udf);
+    }
+
+    public async Task AddPreTrigger()
+    {
+        string preTrigger = """
+                            function addLabel() {
+                                var context = getContext();
+                                var request = context.getRequest();
+                                
+                                var pendingItem = request.getBody();
+
+                                if (!('label' in pendingItem))
+                                    pendingItem['label'] = 'new';
+
+                                request.setBody(pendingItem);
+                            }
+                            """;
+        
+        TriggerProperties trigger = CreatePreTrigger("addLabel", preTrigger);
+        Container container = GetContainer<Person>();
+        
+        await container.Scripts.CreateTriggerAsync(trigger);
+    }
+
+    /// <summary>
+    /// Creates a Pre-Trigger from a name and body.
+    /// </summary>
+    /// <param name="id">The identifier of the trigger, used to invoke it in queries.</param>
+    /// <param name="body">The JavaScript implementation of the trigger.</param>
+    /// <returns>A <see cref="TriggerProperties"/> object containing the definition of the trigger, to be added to the scripts of a container.</returns>
+    /// <remarks>
+    /// <para>Triggers are the core way that Azure Cosmos DB for NoSQL can inject business logic both before and after operations.</para>
+    /// <para>Triggers are defined as JavaScript functions. The function is then executed when the trigger is invoked.</para>
+    /// </remarks>
+    /// <example>
+    /// The following example shows how to create a trigger that adds a property 'label' if it is not already present.
+    /// <code>
+    /// function addLabel() {
+    ///     var context = getContext();
+    ///     var request = context.getRequest();
+    /// 
+    ///     var pendingItem = request.getBody();
+    /// 
+    ///     if (!('label' in pendingItem))
+    ///     pendingItem['label'] = 'new';
+    /// 
+    ///     request.setBody(pendingItem);
+    /// </code>
+    /// </example>
+    public TriggerProperties CreatePreTrigger(string id, string body)
+    {
+        return new TriggerProperties
+        {
+            Id = id,
+            Body = body,
+            TriggerOperation = TriggerOperation.Create,
+            TriggerType = TriggerType.Pre
+        };
     }
     
     public async Task<List<string>> QueryUserDefinedFunction()
@@ -126,7 +188,26 @@ public class CosmosContext : DbContext
 
         return results;
     }
-    
+
+    public async Task UsePreTrigger()
+    {
+        Person bob = new Person
+        {
+            Id = Guid.NewGuid(),
+            Name = "Bob",
+            PartitionKey = nameof(Person),
+        };
+        
+        Container container = GetContainer<Person>();
+        
+        ItemRequestOptions options = new()
+        {
+            PreTriggers = new List<string> { "addLabel" },
+        };
+        
+        await container.CreateItemAsync(bob, requestOptions: options );
+    }
+
     /// <summary>
     /// Uses reflection to get the name of the DbSet property where the entity is stored.
     /// </summary>
