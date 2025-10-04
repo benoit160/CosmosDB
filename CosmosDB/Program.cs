8eecbcc13763fb745ffa97a9b500aa7e7c414036
Benoît <benoit160@live.be>
2025-10-04 16:36:25 +0200

    wip

--- CosmosDB/Program.cs
+++ CosmosDB/Program.cs
@@ -1,9 +1,47 @@
 ﻿namespace CosmosDB;
 
+using Microsoft.Azure.Cosmos;
+using Microsoft.EntityFrameworkCore;
+
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main()
     {
-        Console.WriteLine("Hello, World!");
+        CosmosContext context = new CosmosContext();
+        CosmosClient client = context.Database.GetCosmosClient();
+
+        Container sourceContainer = client.GetContainer("Sandbox", "Container");
+        Container leaseContainer = client.GetContainer("Sandbox", "ChangeFeed");
+
+        Container.ChangesHandler<Person> changeHandlerDelegate = async (changes, _) => 
+        {
+            foreach(Person person in changes)
+            {
+                await ValueTask.CompletedTask;
+                Console.WriteLine($"change processor is processing person with name : {person.Name}");
+            }
+        };
+
+        ChangeFeedProcessorBuilder builder = sourceContainer.GetChangeFeedProcessorBuilder("Processor", changeHandlerDelegate);
+
+        ChangeFeedProcessor processor = builder
+            .WithInstanceName("desktopApplication")
+            .WithLeaseContainer(leaseContainer)
+            .Build();
+        
+        await processor.StartAsync();
+        
+        Person person = new Person
+        {
+            PartitionKey = nameof(Person),
+            Name = "Bob" 
+        };
+        
+        context.Persons.Add(person);
+        await context.SaveChangesAsync();
+        
+        Console.ReadLine();
+        
+        await processor.StopAsync();
     }
 }
